897e772eabcc3e9a034db34f030465c5042cc812
천새벽 <lazyson0820@gmail.com>
2020-06-25 22:48:46 +0900

    helloSpringBoot

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("MacbookAir", "노트북", 1200000, "Apple", 10, "맥북에어")
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("MacbookPro", "노트북", 2000000, "Apple", 5, "맥북프로")
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("Gram", "노트북", 1500000, "LG", 20, "LG그램")
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("Always", "노트북", 1000000, "SAMSUNG", 10, "삼성올웨이즈")
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("삼성TV", "가전", 2000000, "SAMSUNG", 8, "최고급TV")
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("LGTV", "가전", 1500000, "LG", 10, "중급형TV")
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("AirMax", "잡화", 150000, "Nike", 50, "나이키 에어맥스")
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("YeezyBoost", "잡화", 400000, "Adidas", 30, "아디다스 이지부스트")
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("티셔츠", "잡화", 20000, "ZARA", 100, "ZARA 티셔츠")
+insert into product(name, category, price, manufacturer, unitInStock, description) values ("청바지", "잡화", 50000, "ZARA", 80, "ZARA 청바지")
+
+
